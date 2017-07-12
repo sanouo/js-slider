d61ef7d355948fa312dffc2a52de17894554d8af
sanouo <sanouo591@gmail.com>
2017-07-12 11:24:36 +0200

    create html

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var picture = document.getElementsByClassName("picture");
+var array = ["ciel.jpg", "lune.jpg", "montagne.jpg", "tempete.jpg"];
+
+for (let i = 0; i < array.length; i++){
+  alert(i);
+}
